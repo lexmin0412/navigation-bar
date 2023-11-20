48bdc9caca94ab37ccaa71c1a74661bb332049aa
lexmin0412 <zhangle_dev@outlook.com>
2023-11-20 23:52:17 +0800

    feat: 新增打招呼呀

--- newtab.tsx
+++ newtab.tsx
@@ -15,20 +15,32 @@ function IndexNewtab() {
         width: "100vw",
         height: "100vh",
         display: "flex",
-				alignItems: 'center',
-				justifyContent: 'center',
-				flexDirection: 'column',
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
         padding: 16,
         background:
           "url(https://lexmin.oss-cn-hangzhou.aliyuncs.com/statics/browser-extensions/yuai.jpg)"
       }}>
-      <h1
-        style={{
-          color: "white"
-        }}>
-        欢迎来到 Panda 星球
-      </h1>
-      <div>今天是 {currentDate}</div>
+      <div
+				style={{
+					textAlign: 'center',
+					background: 'rgba(0, 0,0,0.3)',
+					borderRadius: '8px',
+					padding: '20px'
+				}}
+			>
+        <h1>Hello 小黄</h1>
+        <h1
+          style={{
+            color: "white"
+          }}>
+          欢迎来到 Panda 星球
+        </h1>
+        <div style={{
+					fontSize: '16px'
+				}}>今天是 {currentDate}</div>
+      </div>
     </div>
   )
 }
