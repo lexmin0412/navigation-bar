255febb0297a35a5ae1535c8a242fe8e4ca0b7c0
lexmin0412 <zhangle_dev@outlook.com>
2023-11-21 00:24:44 +0800

    feat(newtab): 背景图支持动态数据及轮播

--- newtab.tsx
+++ newtab.tsx
@@ -1,35 +1,95 @@
-import { useState } from "react"
+import { Carousel } from "antd";
+import { useEffect, useState } from "react";
+
 
 function IndexNewtab() {
-  const [data, setData] = useState("")
+  const [imgList, setImgList] = useState([
+    {
+      url: "https://lexmin.oss-cn-hangzhou.aliyuncs.com/statics/browser-extensions/yuai.jpg",
+      name: "渝爱"
+    },
+    {
+      url: "https://lexmin.oss-cn-hangzhou.aliyuncs.com/statics/browser-extensions/src%3Dhttp___safe-img.xhscdn.com_bw1_185b6252-f3f1-4f9f-ba03-7b10bad853ef_imageView2_2_w_1080_format_jpg%26refer%3Dhttp___safe-img.xhscdn.webp",
+      name: "福宝"
+    },
+    {
+      url: "https://lexmin.oss-cn-hangzhou.aliyuncs.com/statics/browser-extensions/src%3Dhttp___safe-img.xhscdn.com_bw1_576dc3ec-016c-4809-806d-c861feb20d26_imageView2_2_w_1080_format_jpg%26refer%3Dhttp___safe-img.xhscdn.webp",
+      name: "福宝"
+    },
+    {
+      url: "https://lexmin.oss-cn-hangzhou.aliyuncs.com/statics/browser-extensions/src%3Dhttp___safe-img.xhscdn.com_bw1_8a00941f-56c3-4b33-96db-425540b48abd_imageView2_2_w_1080_format_jpg%26refer%3Dhttp___safe-img.xhscdn.webp",
+      name: "福宝"
+    },
+    {
+      url: "https://lexmin.oss-cn-hangzhou.aliyuncs.com/statics/browser-extensions/u%3D167677896%2C2615296049%26fm%3D253%26fmt%3Dauto%26app%3D138%26f%3DJPEG.webp",
+      name: "福宝"
+    },
+    {
+      url: "https://lexmin.oss-cn-hangzhou.aliyuncs.com/statics/browser-extensions/u%3D2903620256%2C2383358246%26fm%3D253%26fmt%3Dauto%26app%3D138%26f%3DJPEG.webp",
+      name: "福宝"
+    }
+  ])
   const year = new Date().getFullYear()
   const month = new Date().getMonth() + 1
   const date = new Date().getDate()
   const currentDate = `${year}-${month}-${date}`
 
+
+	const initImgList = async() => {
+		fetch(
+			"https://lexmin.oss-cn-hangzhou.aliyuncs.com/statics/browser-extensions/pandas.json"
+		).then(res=>{
+			return res.json()
+		}).then((res)=>{
+			console.log('resss', res)
+			if (res?.length) {
+				setImgList(res)
+			}
+		})
+	}
+
+
+	useEffect(()=>{
+		initImgList()
+	}, [])
+
   return (
-    <div
-      style={{
-        color: "white",
-        boxSizing: "border-box",
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        padding: 16,
-        background:
-          "url(https://lexmin.oss-cn-hangzhou.aliyuncs.com/statics/browser-extensions/yuai.jpg)"
-      }}>
-      <div
-				style={{
-					textAlign: 'center',
-					background: 'rgba(0, 0,0,0.3)',
-					borderRadius: '8px',
-					padding: '20px'
-				}}
-			>
+		<div>
+			<Carousel autoplay>
+				{imgList.map((item) => {
+					return (
+						<div
+							style={{
+								height: "100vh",
+								width: "100vw"
+							}}>
+							<img
+								style={{
+									width: "100vw",
+									height: "100vh",
+									display: "block",
+									objectFit: 'cover',
+								}}
+								src={item.url}
+							/>
+						</div>
+					)
+				})}
+			</Carousel>
+
+			<div
+    		style={{
+					position: 'absolute',
+					top: '50%',
+					left: '50%',
+					transform: 'translate(-50%, -50%)',
+    			textAlign: 'center',
+    			background: 'rgba(0, 0,0,0.4)',
+    			borderRadius: '8px',
+    			padding: '20px',
+					color: '#fff'
+    		}}
+    	>
         <h1>Hello 小黄</h1>
         <h1
           style={{
@@ -38,10 +98,10 @@ function IndexNewtab() {
           欢迎来到 Panda 星球
         </h1>
         <div style={{
-					fontSize: '16px'
-				}}>今天是 {currentDate}</div>
+    			fontSize: '16px'
+    		}}>今天是 {currentDate}</div>
       </div>
-    </div>
+		</div>
   )
 }
 
