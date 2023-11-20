90f2db16f96a89492ad7953d9ec871fe33a3e241
lexmin0412 <zhangle_dev@outlook.com>
2023-11-20 22:37:20 +0800

    feat: 修改弹窗内容, 新增网站列表

--- popup.tsx
+++ popup.tsx
@@ -1,20 +1,63 @@
-import { useState } from "react"
-import './popup.css'
+import { useState } from "react";
+
+import './popup.css';
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+
+	const websites = [
+    {
+      url: "https://lexmin0412.github.io/my-second-brain",
+      title: "My Second Brain"
+    },
+    {
+      url: "https://lexmin0412.github.io/advanced-stack-park/",
+      title: "Advanced Stack Park"
+    },
+    {
+      url: "https://lexmin0412.github.io/smart-calculator/",
+      title: "房贷计算器"
+    },
+    {
+      url: "https://docs.cellerchan.top/",
+      title: "家庭日历"
+    }
+  ]
 
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        padding: 16
+        padding: 16,
+				width: '200px',
       }}>
       <h1>
-				Click <a href="https://lexmin0412.github.io/advanced-stack-park/">Here</a> to Home.
+				一起去星辰大海。
       </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
+			<ul>
+				{
+					websites.map((item)=>{
+						return (
+							<li style={{
+								height: '32px',
+								lineHeight: '32px'
+							}}>
+								<a
+									style={{
+										color: '#4588ff',
+										textDecoration: 'none'
+									}}
+									href={item.url}
+									title={item.title}
+									target='_blank'
+								>
+									{item.title}
+								</a>
+							</li>
+						)
+					})
+				}
+			</ul>
 			<div className="copyright">Created by <a className="author-name"
 				href="https://github.com/lexmin0412"
 			>Lexmin0412</a>.</div>
